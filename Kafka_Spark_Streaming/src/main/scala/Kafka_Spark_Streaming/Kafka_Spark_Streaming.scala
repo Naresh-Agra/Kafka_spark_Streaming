581d225b9462ec6859f7925d3a7fd2f3c9e7530b
Naresh-Agra <Naresh-Agra@users.noreply.github.com>
2020-01-17 11:09:55 +0530

    Kafka

--- Kafka_Spark_Streaming/src/main/scala/Kafka_Spark_Streaming/Kafka_Spark_Streaming.scala
+++ Kafka_Spark_Streaming/src/main/scala/Kafka_Spark_Streaming/Kafka_Spark_Streaming.scala
@@ -2,38 +2,57 @@ package Kafka_Spark_Streaming
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.StructField
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.types._
-import org.apache.spark.sql._
-import scala.io.Source
-import org.apache.spark.sql.functions.explode
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.jdbc._
-import com.mysql.cj.jdbc.Driver
-import com.databricks.spark.xml._
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming._
 
 object Kafka_Spark_Streaming {
     def main(args:Array[String]):Unit={
-    
+      
+    println("Spark Execution Started")
     val conf=new SparkConf().setAppName("Spark_XML1").setMaster("local[*]").set("spark.driver.allowMultipleContexts","true")
     val sc=new SparkContext(conf)
     sc.setLogLevel("ERROR")
-    
     val spark=SparkSession
               .builder()
               .config(conf)
               .getOrCreate()
               
     import spark.implicits._
+   
+    val kafkaParams = Map[String, Object](
+                    "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
+                    "key.deserializer" -> classOf[StringDeserializer],
+                    "value.deserializer" -> classOf[StringDeserializer],
+                    "group.id" -> "stream_id",
+                    "auto.offset.reset" -> "latest"
+                    )
     
-    
-    
-    
-    
+    val ssc = new StreamingContext(conf,Seconds(2))
+    val topics = Array("kafkastruct01")
+    val stream = KafkaUtils.createDirectStream[String, String](
+                  ssc,
+                  PreferConsistent,
+                  Subscribe[String, String](topics, kafkaParams)
+                  )
+
+    val dstreams=stream.map(x => x.value)
+    dstreams.print()
+    dstreams.foreachRDD(x=>
+    if(!x.isEmpty())
+  {
+
+    val flattendata=x.flatMap(x=>x.split(","))
+    flattendata.foreach(println)
+
+}
+)
+    ssc.start()
+    ssc.awaitTermination()
+
     
     }
   
